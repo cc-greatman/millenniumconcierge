f6e766608e865c9b8cbc91ecfbcaf006fdb03fec
Greatman Blessed <blessedgreatman96@gmail.com>
2025-01-03 07:31:25 +0100

    `Fixed comparison operators in membership type conditionals`

--- resources/views/user/membership/setting.blade.php
+++ resources/views/user/membership/setting.blade.php
@@ -134,17 +134,17 @@
                                       <div class="d-flex align-items-center">
                                         <div class="flex-grow-1 ms-3">
                                           <h6 class="mb-0">
-                                            @if($payment->membership_type ===  1)
+                                            @if($payment->membership_type ==  1)
                                                 SILVER MEMBERSHIP
-                                            @elseif($payment->membership_type ===  2)
+                                            @elseif($payment->membership_type ==  2)
                                                 GOLD MEMBERSHIP
-                                            @elseif($payment->membership_type ===  3)
+                                            @elseif($payment->membership_type ==  3)
                                                 PLATINUM MEMBERSHIP
                                             @endif</h6>
                                         </div>
                                       </div>
                                     </td>
-                                    <td>@if($payment->membership_type ===  1)&#8358;@else$@endif{{ number_format($payment->amount, 2) }}</td>
+                                    <td>@if($payment->membership_type ==  1)&#8358;@else$@endif{{ number_format($payment->amount, 2) }}</td>
                                     <td>{{ $payment->payment_id }}</td>
                                     <td>{{ $payment->created_at->format('Y-m-d H:i') }}</td>
                                     @if ($payment->status === "completed")
