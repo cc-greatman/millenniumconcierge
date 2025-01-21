0590ff2b925646178144b7c37f0cb1e5afc764e6
Greatman Blessed <131499004+cc-greatman@users.noreply.github.com>
2025-01-21 22:21:04 +0100

    Update ViewController.php

--- app/Http/Controllers/Admin/ViewController.php
+++ app/Http/Controllers/Admin/ViewController.php
@@ -221,7 +221,7 @@ class ViewController extends Controller
         return view('admin.trips.hotels.completed', compact('pageTitle', 'bookings', 'sum'));
     }
 
-    public function pendingBooking() {
+    public function pendingBookings() {
 
         $pageTitle = "Pending Hotel Trips || ". env('APP_NAME');
 
