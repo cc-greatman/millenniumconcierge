590c5c68c5e5e99db13aa19feb712bd6db65c238
Greatman Blessed <blessedgreatman96@gmail.com>
2025-01-23 08:48:10 +0100

    `Update navbar.blade.php to display session currency and change icon`

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -40,7 +40,7 @@
                     aria-haspopup="false"
                     aria-expanded="false"
                 >
-                    <i class="material-icons-two-tone">swap_vertical_circle</i>$
+                    <i class="ph-duotone ph-arrows-counter-clockwise"></i>{{ session('currency') }}
                 </a>
                 <div class="dropdown-menu dropdown-menu-end pc-h-dropdown">
                     <form action="{{ route('currency.change') }}" method="POST" id="currency-form">
