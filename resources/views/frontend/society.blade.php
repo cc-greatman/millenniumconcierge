81be2ea363dadcaa496335e584fd772d02191975
Greatman Blessed <blessedgreatman96@gmail.com>
2025-02-26 12:02:21 +0100

    `Removed star rating and updated section subtitle in society.blade.php`

--- resources/views/frontend/society.blade.php
+++ resources/views/frontend/society.blade.php
@@ -39,14 +39,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 mb-30 animate-box" data-animate-effect="fadeInUp">
-                <span>
-                    <i class="star-rating"></i>
-                    <i class="star-rating"></i>
-                    <i class="star-rating"></i>
-                    <i class="star-rating"></i>
-                    <i class="star-rating"></i>
-                </span>
-                <div class="section-subtitle">The Cappa Luxury Hotel</div>
+                <div class="section-subtitle">The Millennium Society - <em>for the one percent of the one percent</em> </div>
                 <div class="section-title">Enjoy a Luxury Experience</div>
                 <p>Welcome to the best five-star deluxe hotel in New York. Hotel elementum sesue the aucan vestibulum aliquam justo in sapien rutrum volutpat. Donec in quis the pellentesque velit. Donec id velit ac arcu posuere blane.</p>
                 <p>Hotel ut nisl quam nestibulum ac quam nec odio elementum sceisue the aucan ligula. Orci varius natoque penatibus et magnis dis parturient monte nascete ridiculus mus nellentesque habitant morbine.</p>
