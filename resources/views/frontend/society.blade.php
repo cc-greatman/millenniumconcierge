8652fc6cb4a97a9b2e51b132a09484fb4715d164
Greatman Blessed <blessedgreatman96@gmail.com>
2025-02-26 13:21:25 +0100

    `Updated text in society.blade.php: changed "fulfilled" to "met" in a paragraph.`

--- resources/views/frontend/society.blade.php
+++ resources/views/frontend/society.blade.php
@@ -302,7 +302,7 @@
                 <div class="single-facility animate-box" data-animate-effect="fadeInUp">
                     <span class="flaticon-bed"></span>
                     <h5>Top Luxury Services</h5>
-                    <p> From private aviation to bespoke lifestyle management, every need is fulfilled with precision and discretion.</p>
+                    <p> From private aviation to lifestyle management, every need is met with precision and discretion.</p>
                     <div class="facility-shape"> <span class="flaticon-bed"></span> </div>
                 </div>
             </div>
