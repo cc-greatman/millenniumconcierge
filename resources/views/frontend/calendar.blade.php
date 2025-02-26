77daee67854396bf3518c09b23cc930d91f1e49b
Greatman Blessed <blessedgreatman96@gmail.com>
2025-02-26 14:48:28 +0100

    `Updated calendar view with new dates and locations`

--- resources/views/frontend/calendar.blade.php
+++ resources/views/frontend/calendar.blade.php
@@ -23,15 +23,15 @@
         <div class="row">
             <div class="col-md-4 mb-30">
                 <div class="item">
-                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/1.jpg") }}" alt="">
+                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/3.jpg") }}" alt="">
                             <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>02</i> </a>
+                                <a href="javascript:void(0);"> <span>Aug</span> <i>2025</i> </a>
                             </div>
                         </div>
                         <div class="con"> <span class="category">
-                                <a href="news.html">Restaurant</a>
+                                <a href="javascript:void(0);">Dubai, UAE</a>
                             </span>
-                            <h5><a href="post.html">Historic restaurant renovated</a></h5>
+                            <h5><a href="javascript:void(0);">Dubai Business Retreat</a></h5>
                         </div>
                     </div>
             </div>
@@ -39,70 +39,27 @@
                 <div class="item">
                         <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/2.jpg") }}" alt="">
                             <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>04</i> </a>
+                                <a href="javascript:void(0);"> <span>Nov</span> <i>2025</i> </a>
                             </div>
                         </div>
                         <div class="con"> <span class="category">
-                                <a href="news.html">Spa</a>
+                                <a href="javascript:void(0);">Thailand</a>
                             </span>
-                            <h5><a href="post.html">Benefits of Spa Treatments</a></h5>
+                            <h5><a href="javascript:void(0);">Asian Pacific Business Roundtable</a></h5>
                         </div>
                     </div>
             </div>
             <div class="col-md-4 mb-30">
                 <div class="item">
-                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/3.jpg") }}" alt="">
+                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/1.jpg") }}" alt="">
                             <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>06</i> </a>
+                                <a href="javascript:void(0);"> <span>Dec</span> <i>2025</i> </a>
                             </div>
                         </div>
                         <div class="con"> <span class="category">
-                                <a href="news.html">Bathrooms</a>
+                                <a href="javascript:void(0);">Capetown, South Africa</a>
                             </span>
-                            <h5><a href="post.html">Hotel Bathroom Collections</a></h5>
-                        </div>
-                    </div>
-            </div>
-            <div class="col-md-4 mb-30">
-                <div class="item">
-                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/4.jpg") }}" alt="">
-                            <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>08</i> </a>
-                            </div>
-                        </div>
-                        <div class="con"> 
-                            <span class="category">
-                                <a href="news.html">Health</a>
-                            </span>
-                            <h5><a href="post.html">Weight Loss with Fitness Health Club</a></h5>
-                        </div>
-                    </div>
-            </div>
-            <div class="col-md-4 mb-30">
-                <div class="item">
-                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/6.jpg") }}" alt="">
-                            <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>08</i> </a>
-                            </div>
-                        </div>
-                        <div class="con"> <span class="category">
-                                <a href="news.html">Design</a>
-                            </span>
-                            <h5><a href="post.html">Retro Lighting Design in The Hotels</a></h5>
-                        </div>
-                    </div>
-            </div>
-            <div class="col-md-4 mb-30">
-                <div class="item">
-                        <div class="position-re o-hidden"> <img src="{{ asset("../frontend/img/news/5.jpg") }}" alt="">
-                            <div class="date">
-                                <a href="post.html"> <span>Dec</span> <i>08</i> </a>
-                            </div>
-                        </div>
-                        <div class="con"> <span class="category">
-                                <a href="news.html">Health</a>
-                            </span>
-                            <h5><a href="post.html">Benefits of Swimming for Your Health</a></h5>
+                            <h5><a href="javascript:void(0);">High Networth Convergence</a></h5>
                         </div>
                     </div>
             </div>
@@ -111,9 +68,9 @@
             <div class="col-md-12 text-center">
                 <!-- Pagination -->
                 <ul class="news-pagination-wrap align-center mb-30 mt-30">
-                    <li><a href="news.html#"><i class="ti-angle-left"></i></a></li>
-                    <li><a href="news.html#" class="active">1</a></li>
-                    <li><a href="news.html#"><i class="ti-angle-right"></i></a></li>
+                    <li><a href="javascript:void(0);"><i class="ti-angle-left"></i></a></li>
+                    <li><a href="javascript:void(0);" class="active">1</a></li>
+                    <li><a href="javascript:void(0);"><i class="ti-angle-right"></i></a></li>
                 </ul>
             </div>
         </div>
