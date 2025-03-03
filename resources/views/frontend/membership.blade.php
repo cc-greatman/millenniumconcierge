6ea396ec1bfb1d64a37e5e25f90f560310f88416
Greatman Blessed <blessedgreatman96@gmail.com>
2025-03-03 16:11:32 +0100

    `Added membership charges and descriptions to membership plans`

--- resources/views/frontend/membership.blade.php
+++ resources/views/frontend/membership.blade.php
@@ -34,6 +34,7 @@
                                 background-clip: text;
                                 -text-fill-color: transparent;
                             ">Silver</a></h4>
+                            <p class="service-text"><strong style="font-weight: 700;">This membership charges: ₦70,000 annually</strong></p>
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text">* Discounted hotel rates</p>
                             <p class="service-text">* Itinerary management and flight booking at no extra fee</p>
@@ -58,6 +59,7 @@
                                 background-clip: text;
                                 -text-fill-color: transparent;
                             ">Gold</a></h4>
+                            <p class="service-text"><strong style="font-weight: 700;">This membership charges: ₦100,000 annually</strong></p>
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text"><strong style="font-weight: 700; color: #fff">All Silver Membership offers plus:</strong></p>
                             <p class="service-text">* Free access to airport exclusive lounge</p>
@@ -83,17 +85,18 @@
                                 background-clip: text;
                                 -text-fill-color: transparent;
                             ">Millennium Society</a></h4>
+                            <p class="service-text"><strong style="font-weight: 700;">This membership charges: Available on Private Enquiry</strong></p>
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text"><strong style="font-weight: 700; color: #fff">All Gold & Silver Membership offers plus:</strong></p>
                             <p class="service-text" style="margin-top: -10px;">* Access to Private Jet Charter at no extra charge</p>
-                            <p class="service-text">* E introduction to all Platinum Members</p>
+                            <p class="service-text">* E introduction to all Society Members</p>
                             <p class="service-text" style="margin-top: -10px;">* B 2 B meeting set up</p>
                             <p class="service-text" style="margin-top: -10px;">* International Speaking opportunity</p>
                             <p class="service-text" style="margin-top: -10px;">* International award nomination</p>
                             <p class="service-text" style="margin-top: -10px;">* Access to Exclusive High Value Domestic and International Events</p>
                         </div>
                         <div class="service-actions">
-                            <a href="{{ route('contact') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
+                            <a href="{{ route('society') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
