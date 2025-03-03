7d74e8095374f0787a2fbc468a7239a04e3d4389
Greatman Blessed <blessedgreatman96@gmail.com>
2025-03-03 16:02:56 +0100

    `Updated membership plan details and registration links in membership.blade.php`

--- resources/views/frontend/membership.blade.php
+++ resources/views/frontend/membership.blade.php
@@ -37,10 +37,9 @@
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text">* Discounted hotel rates</p>
                             <p class="service-text">* Itinerary management and flight booking at no extra fee</p>
-                            <p class="service-text">* Free access to airport exclusive lounge</p>
                         </div>
                         <div class="service-actions">
-                            <a href="{{ route('contact') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
+                            <a href="{{ route('auth.register.show') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
@@ -61,13 +60,11 @@
                             ">Gold</a></h4>
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text"><strong style="font-weight: 700; color: #fff">All Silver Membership offers plus:</strong></p>
-                            <p class="service-text">* Access to Private Jet Charter at no extra charge</p>
-                            <p class="service-text" style="margin-top: -10px;">* Access to Exclusive High Value Domestic and
-                                International Events</p>
+                            <p class="service-text">* Free access to airport exclusive lounge</p>
                             <p class="service-text" style="margin-top: -10px;">* Access to 24/7 customer service</p>
                         </div>
                         <div class="service-actions">
-                            <a href="{{ route('contact') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
+                            <a href="{{ route('auth.register.show') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
                         </div>
                     </div>
                 </div>
@@ -76,7 +73,7 @@
                 <div class="services4">
                     <div class="service-img"><img src="{{ asset("../frontend/img/news/5.jpg") }}" alt="" class="w-100"></div>
                     <div class="service-header">
-                        <h3 class="service-label">Platinum Membership</h3>
+                        <h3 class="service-label">Millennium Society</h3>
                     </div>
                     <div class="service-wrap">
                         <div class="service-cont">
@@ -85,13 +82,15 @@
                                 -webkit-text-fill-color: transparent;
                                 background-clip: text;
                                 -text-fill-color: transparent;
-                            ">Platinum</a></h4>
+                            ">Millennium Society</a></h4>
                             <p class="service-text"><strong style="font-weight: 700;">This membership offers:</strong></p>
                             <p class="service-text"><strong style="font-weight: 700; color: #fff">All Gold & Silver Membership offers plus:</strong></p>
+                            <p class="service-text" style="margin-top: -10px;">* Access to Private Jet Charter at no extra charge</p>
                             <p class="service-text">* E introduction to all Platinum Members</p>
                             <p class="service-text" style="margin-top: -10px;">* B 2 B meeting set up</p>
                             <p class="service-text" style="margin-top: -10px;">* International Speaking opportunity</p>
-                            <p class="service-text" style="margin-top: -10px;">*  International award nomination</p>
+                            <p class="service-text" style="margin-top: -10px;">* International award nomination</p>
+                            <p class="service-text" style="margin-top: -10px;">* Access to Exclusive High Value Domestic and International Events</p>
                         </div>
                         <div class="service-actions">
                             <a href="{{ route('contact') }}" class="icon-btn"><i class="ti-angle-right"></i></a>
