30de9c6313d68cfcfb0eb7805e64f17d5a2ab7f3
Greatman Blessed <blessedgreatman96@gmail.com>
2025-03-16 21:57:12 +0100

    `Update route in side-footer partial from gallery to calendar`

--- resources/views/admin/auth/partials/side-footer.blade.php
+++ resources/views/admin/auth/partials/side-footer.blade.php
@@ -9,7 +9,7 @@
         <ul class="list-inline footer-link mb-0">
           <li class="list-inline-item"><a href="{{ route('home') }}">Home</a></li>
           <li class="list-inline-item"><a href="{{ route('about') }}">About</a></li>
-          <li class="list-inline-item"><a href="{{ route('gallery') }}">Gallery</a></li>
+          <li class="list-inline-item"><a href="{{ route('calendar') }}">Calendar</a></li>
           <li class="list-inline-item"><a href="{{ route('services') }}">Services</a></li>
           <li class="list-inline-item"><a href="{{ route('membership') }}">Membership</a></li>
           <li class="list-inline-item"><a href="{{ route('contact') }}">Enquiry</a></li>
